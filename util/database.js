a4f141db1466fde2315a6321f0db6b8f36b9e92f
kobzarmariia <mariia.kobzar@gmail.com>
2020-01-10 15:22:25 +0200

    remove pas

--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = (callback) => {
   MongoClient
-  .connect('mongodb+srv://mariakobzar:livihu90@cluster0-3ymr1.mongodb.net/shop?retryWrites=true&w=majority')
+  .connect('mongodb+srv://***:***@cluster0-3ymr1.mongodb.net/shop?retryWrites=true&w=majority')
   .then(client => {
    console.log('Connected!');
    _db = client.db();
