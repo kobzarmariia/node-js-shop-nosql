bf8ff951402d56b4272cdc554c510f30a799caea
kobzarmariia <mariia.kobzar@gmail.com>
2020-01-20 18:19:48 +0200

    [+] authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,48 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false
+    errorMessage: req.flash('error').join(),
   });
 };
-
+ 
 exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false
+    errorMessage: req.flash('error').join(),
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({email: email})
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
+      if(!user) {
+        req.flash('error', 'Invalid email or password!');
+        return res.redirect('/login');
+      }
+      bcrypt.compare(password, user.password)
+      .then(doMatch => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save(err => { 
+            console.log(err);
+            res.redirect('/');
+          });
+        }
+        req.flash('error', 'Invalid email or password!');
+        res.redirect('/login');
+      })
+      .catch(err => {
         console.log(err);
-        res.redirect('/');
+        res.redirect('/login');
       });
     })
     .catch(err => console.log(err));
@@ -33,22 +52,26 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  console.log(email);
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
+        req.flash('error', 'E-Mail exists already, please pick a different one!');
         return res.redirect('/signup');
       }
-      const user = new User({
-        name: 'Maria',
-        email: email,
-        password: password,
-        cart: { items: [] }
-      });
-      return user.save();
-    })
-    .then(result => {
-      res.redirect('/login');
+      return bcrypt
+      .hash(password, 12)
+      .then(hashedPassword => {    
+        const user = new User({
+          name: 'Maria',
+          email: email,
+          password: hashedPassword,
+          cart: { items: [] }
+        });
+        return user.save();
+      })
+      .then(result => {
+        res.redirect('/login');
+      })
     })
     .catch(err => {
       console.log(err);
